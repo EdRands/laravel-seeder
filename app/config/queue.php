ea61590e4db4c2554a52021bc185aa7b1718dbc3
Ed Rands <erands@gmail.com>
2014-03-17 11:39:33 -0700

    Include host in IronMQ, and a comment reformat

--- app/config/queue.php
+++ app/config/queue.php
@@ -42,21 +42,22 @@ return array(
         ),
         'iron' => array(
             'driver' => 'iron',
+            'host' => 'mq-aws-us-west-1.iron.io',
             'project' => 'your-project-id',
             'token' => 'your-token',
             'queue' => 'your-queue-name',
         ),
     ),
     /*
-    |--------------------------------------------------------------------------
-    | Failed Queue Jobs
-    |--------------------------------------------------------------------------
-    |
-    | These options configure the behavior of failed queue job logging so you
-    | can control which database and table are used to store the jobs that
-    | have failed. You may change them to any database / table you wish.
-    |
-    */
+      |--------------------------------------------------------------------------
+      | Failed Queue Jobs
+      |--------------------------------------------------------------------------
+      |
+      | These options configure the behavior of failed queue job logging so you
+      | can control which database and table are used to store the jobs that
+      | have failed. You may change them to any database / table you wish.
+      |
+     */
     'failed' => array(
         'database' => 'sqlite',
         'table' => 'failed_jobs',
