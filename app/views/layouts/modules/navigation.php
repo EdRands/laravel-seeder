8930bd620afe569a7eee9850d02d02877757f6d5
Ed Rands <erands@gmail.com>
2014-06-02 19:48:06 -0700

    Let's use the white navbar.

--- app/views/layouts/modules/navigation.php
+++ app/views/layouts/modules/navigation.php
@@ -1,6 +1,6 @@
 <?php
 
-echo Navbar::inverse(null, Navbar::FIX_TOP)
+echo Navbar::create(null, Navbar::FIX_TOP)
     ->with_brand(Config::get('project.name.short', 'Project'), url('/'))
     ->with_menus(Navigation::links(array(
             array('Home', url('/'), null, null, null, 'home')
