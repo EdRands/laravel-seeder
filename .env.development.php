e31c42b9d9135f4ebd44278d647438b61f0e0302
Ed Rands <erands@gmail.com>
2014-06-05 03:10:40 -0700

    Move database passwords to environment files.

---
+++ .env.development.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'MYSQL_PASSWORD' => 'secret',
+    'PGSQL_PASSWORD' => 'secret',
+];
---
+++ .env.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'MYSQL_PASSWORD' => '',
+    'PGSQL_PASSWORD' => '',
+];
