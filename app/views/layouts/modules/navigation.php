6b4cfb2f6b62c0f0bdd7fc58495ac28bdaf8a4bd
Ed Rands <erands@gmail.com>
2014-06-13 15:49:13 -0700

    Use language file for navigation labels. Closes #34

--- app/views/layouts/modules/navigation.php
+++ app/views/layouts/modules/navigation.php
@@ -3,6 +3,6 @@
 echo Navbar::create(null, Navbar::FIX_TOP)
     ->with_brand(Config::get('project.name.short', 'Project'), url('/'))
     ->with_menus(Navigation::links([
-            ['Home', url('/'), null, null, null, 'home']
+            [trans('navigation.home'), url('/'), null, null, null, 'home']
     ]))
     ->collapsible();
