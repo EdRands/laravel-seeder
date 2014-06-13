1451cc57a9a93df982166bc5b998e7c3ad6f0ca1
Ed Rands <erands@gmail.com>
2014-06-13 15:24:49 -0700

    Why did I commit that change? That word was only supposed to be there for a second to force a page reload.

--- app/views/splash.blade.php
+++ app/views/splash.blade.php
@@ -6,6 +6,6 @@
 <div class="container text-center">
     <p><img src="{{ asset('img/sprout.png') }}" height="256" width="256" alt="Sprouted plant"></p>
     <h1>{{ trans('project.name.full') }}</h1>
-    <p class="lead">{{ trans('project.name.full') }} is planted and sprouted. Now let's get growing! Dude!</p>
+    <p class="lead">{{ trans('project.name.full') }} is planted and sprouted. Now let's get growing!</p>
 </div>
 @stop
