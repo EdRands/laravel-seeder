dccabbbe6b145d1fa02e8ffce5ae9ee1216f3b1a
Ed Rands <erands@gmail.com>
2014-06-13 18:14:27 -0700

    Move comments from the end of lines to the line above. Some other miscellaneous code reformatting.

--- app/config/packages/barryvdh/laravel-debugbar/config.php
+++ app/config/packages/barryvdh/laravel-debugbar/config.php
@@ -2,105 +2,117 @@
 
 return [
     /*
-     |--------------------------------------------------------------------------
-     | Debugbar Settings
-     |--------------------------------------------------------------------------
-     |
-     | Debugbar is enabled by default, when debug is set to true in app.php.
-     |
+      |--------------------------------------------------------------------------
+      | Debugbar Settings
+      |--------------------------------------------------------------------------
+      |
+      | Debugbar is enabled by default, when debug is set to true in app.php.
+      |
      */
     'enabled' => \Config::get('app.debug'),
-
     /*
-     |--------------------------------------------------------------------------
-     | Vendors
-     |--------------------------------------------------------------------------
-     |
-     | Vendor files are included by default, but can be set to false.
-     | This can also be set to 'js' or 'css', to only include javascript or css vendor files.
-     | Vendor files are for css: font-awesome (including fonts) en for js: jquery 1.8.3
-     |
+      |--------------------------------------------------------------------------
+      | Vendors
+      |--------------------------------------------------------------------------
+      |
+      | Vendor files are included by default, but can be set to false.
+      | This can also be set to 'js' or 'css', to only include javascript or css vendor files.
+      | Vendor files are for css: font-awesome (including fonts) en for js: jquery 1.8.3
+      |
      */
     'include_vendors' => true,
-
     /*
-     |--------------------------------------------------------------------------
-     | Capture Ajax Requests
-     |--------------------------------------------------------------------------
-     |
-     | The Debugbar can capture Ajax requests and display them. If you don't want this (ie. because of errors),
-     | you can use this option to disable sending the data through the headers.
-     |
+      |--------------------------------------------------------------------------
+      | Capture Ajax Requests
+      |--------------------------------------------------------------------------
+      |
+      | The Debugbar can capture Ajax requests and display them. If you don't want this (ie. because of errors),
+      | you can use this option to disable sending the data through the headers.
+      |
      */
     'capture_ajax' => true,
-
     /*
-     |--------------------------------------------------------------------------
-     | DataCollectors
-     |--------------------------------------------------------------------------
-     |
-     | Enable/disable DataCollectors
-     |
+      |--------------------------------------------------------------------------
+      | DataCollectors
+      |--------------------------------------------------------------------------
+      |
+      | Enable/disable DataCollectors
+      |
      */
     'collectors' => [
-        'phpinfo' => false, // Php version
-        'messages' => true, // Messages
-        'time' => true, // Time Datalogger
-        'memory' => true, // Memory usage
-        'exceptions' => true, // Exception displayer
-        'log' => true, // Logs from Monolog (merged in messages if enabled)
-        'db' => true, // Show database (PDO) queries and bindings
-        'views' => false, // Views with their data
-        'route' => false, // Current route information
-        'laravel' => false, // Laravel version and environment
-        'events' => false, // All events fired
-        'twig' => false, // Twig, requires barryvdh/laravel-twigbridge
-        'default_request' => false, // Regular or special Symfony request logger
-        'symfony_request' => false, // Only one can be enabled..
-        'mail' => false, // Catch mail messages
-        'logs' => false, // Add the latest log messages
-        'files' => false, // Show the included files
-        'config' => false, // Display config settings
+        /* Php version */
+        'phpinfo' => false,
+        /* Messages */
+        'messages' => true,
+        /* Time Datalogger */
+        'time' => true,
+        /* Memory usage */
+        'memory' => true,
+        /* Exception displayer */
+        'exceptions' => true,
+        /* Logs from Monolog (merged in messages if enabled) */
+        'log' => true,
+        /* Show database (PDO) queries and bindings */
+        'db' => true,
+        /* Views with their data */
+        'views' => false,
+        /* Current route information */
+        'route' => false,
+        /* Laravel version and environment */
+        'laravel' => false,
+        /* All events fired */
+        'events' => false,
+        /* Twig, requires barryvdh/laravel-twigbridge */
+        'twig' => false,
+        /* Regular or special Symfony request logger. Only one can be enabled. */
+        'default_request' => false,
+        'symfony_request' => false,
+        /* Catch mail messages */
+        'mail' => false,
+        /* Add the latest log messages */
+        'logs' => false,
+        /* Show the included files */
+        'files' => false,
+        /* Display config settings */
+        'config' => false,
     ],
-
     /*
-     |--------------------------------------------------------------------------
-     | Extra options
-     |--------------------------------------------------------------------------
-     |
-     | Configure some DataCollectors
-     |
+      |--------------------------------------------------------------------------
+      | Extra options
+      |--------------------------------------------------------------------------
+      |
+      | Configure some DataCollectors
+      |
      */
     'options' => [
         'pdo' => [
-            'with_params' => true, // Render SQL with the parameters substituted
-            'quotation_char' => '`', // The character to surround params
-            'extra_connections' => [ //Add extra connections to the PDO Collector
-                // 'mysql',
-            ]
+            /* Render SQL with the parameters substituted */
+            'with_params' => true,
+            /* The character to surround params */
+            'quotation_char' => '`',
+            /* Add extra connections to the PDO Collector */
+            'extra_connections' => []
         ],
         'mail' => [
             'full_log' => false
         ],
-
         'views' => [
             'data' => true,
         ],
         'route' => [
-            'label' => true // show complete route on bar
+            /* Show complete route on bar */
+            'label' => true
         ]
     ],
-
     /*
-     |--------------------------------------------------------------------------
-     | Inject Debugbar in Response
-     |--------------------------------------------------------------------------
-     |
-     | Usually, the debugbar is added just before <body>, by listening to the
-     | Response after the App is done. If you disable this, you have to add them
-     | in your template yourself. See http://phpdebugbar.com/docs/rendering.html
-     |
+      |--------------------------------------------------------------------------
+      | Inject Debugbar in Response
+      |--------------------------------------------------------------------------
+      |
+      | Usually, the debugbar is added just before <body>, by listening to the
+      | Response after the App is done. If you disable this, you have to add them
+      | in your template yourself. See http://phpdebugbar.com/docs/rendering.html
+      |
      */
     'inject' => true,
-
 ];
