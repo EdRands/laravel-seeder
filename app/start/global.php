f80a4cec4fa13c6fbfa4ec701cbca02f35721d96
Ed Rands <erands@gmail.com>
2014-06-19 16:50:46 -0700

    Use moved filters.

--- app/start/global.php
+++ app/start/global.php
@@ -64,14 +64,9 @@ App::down(function() {
 });
 
 /*
-  |--------------------------------------------------------------------------
-  | Require The Filters File
-  |--------------------------------------------------------------------------
-  |
-  | Next we will load the filters file for the application. This gives us
-  | a nice separate location to store our route and application filter
-  | definitions instead of putting them all in the main routes file.
-  |
+ * Other Required Files
  */
 
-require app_path().'/filters.php';
+// Filters are located within our /src directory.
+require_once base_path().'/src/filters.php';
+
