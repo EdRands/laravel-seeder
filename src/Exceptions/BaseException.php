811166217d8871c0aafc6fb936b5f6dbbe3441cd
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2014-06-20 03:29:09 +0200

    Scrutinizer Auto-Fixes
    This patch was automatically generated as part of the following inspection:
    https://scrutinizer-ci.com/g/edrands/laravel-seeder/inspections/b394026a-5156-4177-9aad-16e0cbc8bb3c
    
    Enabled analysis tools:
     - PHP Mess Detector
     - PHP Code Sniffer
     - PHP CS Fixer
     - Security Advisory Checker
     - PHP Code Coverage
     - PHP Lines Of Code
     - PHP PDepend

--- src/Exceptions/BaseException.php
+++ src/Exceptions/BaseException.php
@@ -3,5 +3,5 @@
 namespace EdRands\Seeder\Exceptions;
 
 abstract class BaseException {
-    
+
 }
--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -5,5 +5,5 @@ namespace EdRands\Seeder\Models;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class BaseModel extends Model {
-    
+
 }
