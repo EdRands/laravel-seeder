97fa050369fafc6dd9fa8698abcf1de951373448
Ed Rands <erands@gmail.com>
2014-06-19 17:13:40 -0700

    Move the User model.

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,11 +1,13 @@
 <?php
 
+namespace EdRands\Seeder\Models;
+
 use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends BaseModel implements UserInterface, RemindableInterface {
     use UserTrait, RemindableTrait;
 
     /**
